63606d9d785d1730dbd32a8cc83b8dc4e031bd15
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-07-04 14:23:33 +0500

    relocate css/js to assets folder

--- functions.php
+++ functions.php
@@ -141,7 +141,7 @@ function huda_scripts() {
 	wp_enqueue_style( 'huda-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'huda-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'huda-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'huda-navigation', get_template_directory_uri() . 'assets/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
