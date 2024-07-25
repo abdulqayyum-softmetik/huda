f1c28fe7f95eea3c7b8e9150bf26bbde47399017
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-07-25 17:46:54 +0500

    fix woocommerce default styling, remove normlize css

--- category.php
+++ category.php
@@ -9,9 +9,11 @@ get_header(); ?>
 
 <div class="category-page">
    <div class="container">
-    <h1 class="category-title">
-        <?php echo esc_html( single_cat_title()); // Display the category title ?>
-    </h1>
+        <div class="category-breadcrumb">
+            <h1 class="category-title">
+                <?php echo esc_html( single_cat_title()); // Display the category title ?>
+            </h1>
+        </div>
 
         <?php 
             get_template_part( 'template-parts/content', 'category' );
