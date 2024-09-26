7258abe2f4082470b1a3708b0707f13c90ad7838
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-09-27 00:22:50 +0500

    fix some twaeks

---
+++ inc/theme-updates/updates.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Check for theme updates from Hostinger.
+ */
+function huda_theme_check_for_updates( $transient ) {
+    if ( empty( $transient->checked ) ) {
+        return $transient;
+    }
+
+    // Theme slug (folder name)
+    $theme_slug = 'huda';
+    
+    // Get the current version of the theme
+    $theme = wp_get_theme( $theme_slug );
+    $current_version = $theme->get( 'Version' );
+
+    // URL to the theme-update.json file on Hostinger
+    $remote_version_file = 'https://srv1481-files.hstgr.io/006f26a5e576a379/files/public_html/updates/theme-update.json';
+
+    // Fetch the remote version data
+    $remote_version_info = wp_remote_get( $remote_version_file );
+
+    if ( ! is_wp_error( $remote_version_info ) && wp_remote_retrieve_response_code( $remote_version_info ) === 200 ) {
+        $remote_version_data = json_decode( wp_remote_retrieve_body( $remote_version_info ), true );
+
+        // Compare the versions
+        if ( version_compare( $current_version, $remote_version_data['new_version'], '<' ) ) {
+            $theme_data = array(
+                'theme'       => $theme_slug,
+                'new_version' => $remote_version_data['new_version'],
+                'url'         => $remote_version_data['details_url'],
+                'package'     => $remote_version_data['package'],
+            );
+
+            // Add the update to the transient response
+            $transient->response[ $theme_slug ] = $theme_data;
+        }
+    }
+
+    return $transient;
+}
+add_filter( 'site_transient_update_themes', 'huda_theme_check_for_updates' );
