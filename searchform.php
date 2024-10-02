9314ab7be433632a44395c83264b53bcd7f52183
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-10-02 22:56:45 +0500

    fix search form code

--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@
 ?>
 
 <form class="huda-search-form" id="hudaSearchForm" action="<?php echo esc_url( home_url('/') ) ?>" method="get" autocomplete="off">
-	<label for="search" class="visually-hidden">Search</label>
+	<label for="search" class="visually-hidden"><?php esc_html_e( 'Search', 'huda' ); ?></label>
 	<input type="text" name="s" id="searchInput" onkeyup="fetchResults()" placeholder="<?php esc_attr_e('Search...', 'huda') ?>" value="<?php the_search_query(); ?>" />
 	<input type="submit" alt="Search" class="button"/>
 </form>
