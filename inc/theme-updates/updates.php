79b26fd21bc144886f1a199ec988aa9505434f62
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-10-03 17:17:37 +0500

    fix vesrion isuues

--- inc/theme-updates/updates.php
+++ inc/theme-updates/updates.php
@@ -18,20 +18,30 @@ if ( ! function_exists( 'huda_theme_check_for_updates' ) ) :
         // Fetch the update information
         $remote_version_info = wp_remote_get( $remote_version_file );
 
-        if ( ! is_wp_error( $remote_version_info ) && wp_remote_retrieve_response_code( $remote_version_info ) === 200 ) {
-            $remote_version_data = json_decode( wp_remote_retrieve_body( $remote_version_info ), true );
+        if ( is_wp_error( $remote_version_info ) ) {
+            error_log( 'Theme update check failed: ' . $remote_version_info->get_error_message() );
+            return $transient;
+        }
 
-            if ( isset( $remote_version_data['new_version'] ) && version_compare( $current_version, $remote_version_data['new_version'], '<' ) ) {
-                $theme_data = array(
-                    'theme'       => $theme_slug,
-                    'new_version' => $remote_version_data['new_version'],
-                    'url'         => $remote_version_data['details_url'],
-                    'package'     => $remote_version_data['package'],
-                );
+        $response_code = wp_remote_retrieve_response_code( $remote_version_info );
+        if ( $response_code !== 200 ) {
+            error_log( 'Theme update check failed: Received response code ' . $response_code );
+            return $transient;
+        }
 
-                // Add the update information to the transient
-                $transient->response[ $theme_slug ] = $theme_data;
-            }
+        $remote_version_data = json_decode( wp_remote_retrieve_body( $remote_version_info ), true );
+        if ( isset( $remote_version_data['new_version'] ) && version_compare( $current_version, $remote_version_data['new_version'], '<' ) ) {
+            $theme_data = array(
+                'theme'       => $theme_slug,
+                'new_version' => $remote_version_data['new_version'],
+                'url'         => $remote_version_data['details_url'],
+                'package'     => $remote_version_data['package'],
+            );
+
+            // Add the update information to the transient
+            $transient->response[ $theme_slug ] = $theme_data;
+        } else {
+            error_log( 'No new version available or version comparison failed.' );
         }
 
         return $transient;
