642c765defeda0480f5847c563718364c520b261
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-11-18 13:50:13 +0500

    fix demo import issues

--- inc/demo-content.php
+++ inc/demo-content.php
@@ -17,111 +17,112 @@
  *
  * @return array Configuration array for the Huda theme demo import.
  */
-function huda_import_config() {
-	return [
-		'themeName'   => 'Huda',
-		'themeSlug'   => 'huda',
-		/**
-		 * The 'multipleZip' setting indicates that the demo import functionality
-		 * supports two options:
-		 *
-		 * 1. Full Demo Import ('multipleZip' => false): Imports all home pages
-		 *    and their associated inner pages.
-		 * 2. Single Demo Import ('multipleZip' => true): Allows importing only a
-		 *    specific home page along with its related inner pages.
-		 */
-		'multipleZip' => true,
-		'demoData'    => [
+if ( ! function_exists( 'huda_import_config' ) ) {
+	function huda_import_config() {
+		return [
+			'themeName'   => 'Huda',
+			'themeSlug'   => 'huda',
 			/**
-			 * The 'blog' key in the 'demoData' array represents the slug of the page.
+			 * The 'multipleZip' setting indicates that the demo import functionality
+			 * supports two options:
 			 *
-			 * To set a page as the homepage after demo import, you need to pass
-			 * the page slug as the key. No other settings are required.
-			 *
-			 * For example, in this configuration:
-			 *
-			 * 'home-page-1' => [
-			 *     'name'         => esc_html__( 'Home Page 1', 'huda' ),
-			 *     'previewImage' => 'home1/screenshot.jpg',
-			 *     ...
-			 * ],
-			 *
-			 * Here, 'home-page-1' is the slug of the page that can be used for
-			 * setting the homepage.
-			 * Make sure each key corresponds to the correct page slug so that it can
-			 * be used to set the homepage.
+			 * 1. Full Demo Import ('multipleZip' => false): Imports all home pages
+			 *    and their associated inner pages.
+			 * 2. Single Demo Import ('multipleZip' => true): Allows importing only a
+			 *    specific home page along with its related inner pages.
 			 */
-			'blog' => [
-				'name'          => esc_html__( 'Default Demo', 'huda' ),
-				'previewImage'  => 'https://softmetik.com/demos/wphuda-demos/demo-1-default/screenshot.jpg',
-				'previewUrl'    => 'https://wphuda.softmetik.com',
-				'demoZip'       => 'https://softmetik.com/demos/wphuda-demos/demo-1-default/demo-import.zip',
-
+			'multipleZip' => true,
+			'demoData'    => [
 				/**
-				 * The 'frontPageBlog' key is a special setting used to designate
-				 * the Posts page as the front page.
+				 * The 'blog' key in the 'demoData' array represents the slug of the page.
 				 *
-				 * If you want the Posts page (blog page) to appear as the front page after the demo import, set 'frontPageBlog' => true.
+				 * To set a page as the homepage after demo import, you need to pass
+				 * the page slug as the key. No other settings are required.
 				 *
-				 * If you do not require the Posts page to be the front page, simply
-				 * omit or discard this setting.
+				 * For example, in this configuration:
 				 *
-				 * Example:
-				 * 'frontPageBlog' => true, // Sets the Posts page as the front page.
-				 *
-				 * If this setting is not needed, you can leave it out entirely.
-				 */
-				'frontPageBlog' => true,
-
-				/**
-				 * Associative array of menu locations and their corresponding names. No other configuration is required.
-				 * Format: menu_location => Menu Name.
-				 *
-				 * Example:
-				 * 'menus' => [
-				 *     // Sets 'Main Menu' to the 'main-menu' location.
-				 *     'main-menu' => 'Main Menu',
-				 *
-				 *      // Sets 'Footer Menu' to the 'footer-menu' location.
-				 *     'footer-menu' => 'Footer Menu',
-				 *
-				 *      // Sets 'Social Links Menu' to the 'social-menu' location.
-				 *     'social-menu' => 'Social Links Menu',
+				 * 'home-page-1' => [
+				 *     'name'         => esc_html__( 'Home Page 1', 'huda' ),
+				 *     'previewImage' => 'home1/screenshot.jpg',
+				 *     ...
 				 * ],
-				 */
-				'menus'         => [
-					'main-menu' => 'Main Menu',
-				],
-
-				/**
-				 * Slug for the blog page.
 				 *
-				 * This slug should correspond to a page that is designated to display
-				 * the blog posts. This ensures that the page with this slug is set as
-				 * the blog page.
+				 * Here, 'home-page-1' is the slug of the page that can be used for
+				 * setting the homepage.
+				 * Make sure each key corresponds to the correct page slug so that it can
+				 * be used to set the homepage.
 				 */
-				'blogSlug'      => 'blog',
-				/**
-				 * Array of plugins to install.
-				 * Some example plugins are included here.
-				 * The array key needs to be the same as plugin slug.
-				 * You can also include a bundled plugin here.
-				 */
-				'plugins'       => [
-					'woocommerce' => [
-						'name'     => 'WooCommerce',
-						'source'   => 'wordpress',
-						'filePath' => 'woocommerce/woocommerce.php',
+				'blog' => [
+					'name'          => esc_html__( 'Default Demo', 'huda' ),
+					'previewImage'  => 'https://softmetik.com/demos/wphuda-demos/demo-1-default/screenshot.jpg',
+					'previewUrl'    => 'https://wphuda.softmetik.com',
+					'demoZip'       => 'https://softmetik.com/demos/wphuda-demos/demo-1-default/demo-import.zip',
+
+					/**
+					 * The 'frontPageBlog' key is a special setting used to designate
+					 * the Posts page as the front page.
+					 *
+					 * If you want the Posts page (blog page) to appear as the front page after the demo import, set 'frontPageBlog' => true.
+					 *
+					 * If you do not require the Posts page to be the front page, simply
+					 * omit or discard this setting.
+					 *
+					 * Example:
+					 * 'frontPageBlog' => true, // Sets the Posts page as the front page.
+					 *
+					 * If this setting is not needed, you can leave it out entirely.
+					 */
+					'frontPageBlog' => true,
+
+					/**
+					 * Associative array of menu locations and their corresponding names. No other configuration is required.
+					 * Format: menu_location => Menu Name.
+					 *
+					 * Example:
+					 * 'menus' => [
+					 *     // Sets 'Main Menu' to the 'main-menu' location.
+					 *     'main-menu' => 'Main Menu',
+					 *
+					 *      // Sets 'Footer Menu' to the 'footer-menu' location.
+					 *     'footer-menu' => 'Footer Menu',
+					 *
+					 *      // Sets 'Social Links Menu' to the 'social-menu' location.
+					 *     'social-menu' => 'Social Links Menu',
+					 * ],
+					 */
+					'menus'         => [
+						'main-menu' => 'Main Menu',
 					],
-					'kirki'       => [
-						'name'     => 'Kirki Customizer Framework',
-						'source'   => 'wordpress',
-						'filePath' => 'kirki/kirki.php',
+
+					/**
+					 * Slug for the blog page.
+					 *
+					 * This slug should correspond to a page that is designated to display
+					 * the blog posts. This ensures that the page with this slug is set as
+					 * the blog page.
+					 */
+					'blogSlug'      => 'blog',
+					/**
+					 * Array of plugins to install.
+					 * Some example plugins are included here.
+					 * The array key needs to be the same as plugin slug.
+					 * You can also include a bundled plugin here.
+					 */
+					'plugins'       => [
+						'woocommerce' => [
+							'name'     => 'WooCommerce',
+							'source'   => 'wordpress',
+							'filePath' => 'woocommerce/woocommerce.php',
+						],
+						'kirki'       => [
+							'name'     => 'Kirki Customizer Framework',
+							'source'   => 'wordpress',
+							'filePath' => 'kirki/kirki.php',
+						],
 					],
 				],
 			],
-		],
-	];
+		];
+	}
+	add_filter( 'sd/edi/importer/config', 'huda_import_config' );
 }
-
-add_filter( 'sd/edi/importer/config', 'huda_import_config' );
