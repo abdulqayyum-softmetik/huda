1e6e175d9cfa8d1cdd7a8b86e3c738c9b880adba
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2024-12-31 11:23:04 +0500

    fix 404 template

--- 404.php
+++ 404.php
@@ -13,14 +13,21 @@ get_header();
 
 ?>
 
-	<?php 
-		// 404 content
+<?php
+	// Check if the function exists
+	if (function_exists('render_huda_404_template')) {
+		// Render the 404 template and check if it succeeded
+		if (!render_huda_404_template('404')) {
+			do_action('huda_404'); // Trigger fallback if template rendering failed
+		}
+	} else {
+		// Trigger fallback if the function doesn't exist
 		do_action('huda_404');
-	?>
+	}
+?>
+
 
 <?php
-
-
 	get_footer();
 
 
