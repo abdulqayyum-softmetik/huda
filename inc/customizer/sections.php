d395e4443255a4dde82dd009a381ef92df142331
abdulqayyum-softmetik <abqayyum.dev@gmail.com>
2025-02-12 17:10:29 +0500

    remove button from styles from kirki

--- inc/customizer/sections.php
+++ inc/customizer/sections.php
@@ -105,25 +105,6 @@ new \Kirki\Section(
 );
 
 /* 
-* Huda Buttons Section
-*/
-new \Kirki\Section(
-	'huda_buttons_section',
-	[
-		'title' => esc_html__( 'Buttons', 'huda' ),
-		'panel' => 'huda_global_options',
-		'tabs'  => [
-			'general' => [
-				'label' => esc_html__( 'General', 'huda' ),
-			],
-			'design'  => [
-				'label' => esc_html__( 'Design', 'huda' ),
-			],
-		],
-	]
-);
-
-/* 
 * Huda Blog Layout Section
 */
 new \Kirki\Section(
